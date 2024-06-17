5479c840954d6debd541502e3fdc32257055102c
Jeeruz <jdelacruz.proacc@gmail.com>
2024-06-18 04:54:41 +0800

    Updated Classes, code from gh-pages

--- Models/Class.cs
+++ Models/Class.cs
@@ -5,9 +5,42 @@ using System.Threading.Tasks;
 
 namespace DnDCharacterCalculator.Models
 {
+    //public class Class
+    //{
+    //    public string Name { get; set; }
+    //    public int Hitdie { get; set; }
+    //}
+
+    //or
     public class Class
     {
-        public string Name { get; set; }
-        public int HitDie { get; set; } 
+        private string name;
+        private int hitdie;
+
+        //public string Name
+        //{
+        //    get { return name;}
+        //    set { name = value;}
+        //}
+
+        //public int Hitdie
+        //{
+        //    get { return hitdie;}
+        //    set { hitdie = value;}
+        //}
+
+        //or
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Hitdie
+        {
+            get => hitdie;
+            set => hitdie = value;
+        }
     }
 }
