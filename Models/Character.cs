74b6a53dd410dc390f0261c04f9bff83d19742d4
Jeeruz <jdelacruz.proacc@gmail.com>
2024-06-18 04:49:34 +0800

    Added changes from the gh-pages branch

--- Models/Character.cs
+++ Models/Character.cs
@@ -13,6 +13,11 @@ namespace DnDCharacterCalculator.Models
     {
         public string Name { get; set; }
         public int Level { get; set; }
+        public string CharClass { get; set; }
+        public int Level2 { get; set; }
+        public string CharClass2 { get; set; }
+        public int Level3 { get; set; }
+        public int Class3 { get; set; }
         public int Strength { get; set; }
         public int StrMod { get; set; }
         public int Dexterity { get; set; }
@@ -24,28 +29,47 @@ namespace DnDCharacterCalculator.Models
         public int Wisdom { get; set; }
         public int WisMod { get; set; }
         public int Charisma { get; set; }
-        public int ChaMod{ get; set; }
+        public int ChaMod { get; set; }
         public int MaxHealth { get; set; }
         public int HitDie { get; set; }
 
         private static int Modifier(int stat)
         {
-            int mod = ((stat - 10) / 2); 
+            int mod = ((stat - 10) / 2);
             return mod;
         }
 
-        private static int Health()
+        private static int CalculatedClassHealth(int level, string charclass)
         {
-            //Sources:
-            //https://www.omnicalculator.com/other/hit-points
+            //Hit Points at 1st Levei: 8 + your Constitution modifier
+            //Hit Points at Higher Leveis: 1d8(or 5) + your Constitution 
+            //    modifier per clerie levei after 1st
+            int calculatedHealth = 0;
+
+            if (level == 1)
+            {
+                //calculatedHealth = 5 + Character.ChaMod;
+                //test comment
+            }
+
+            return 10;
+        }
+
+        private static int CalculatedMaxHealth(int level, string character)
+        {
+            //This will be used to calculate max health if player has multiclasses
 
             return 10;
         }
 
         //This is a contructor of player if it received name parameter
-        public Character(string name, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma)
+        public Character(string name, int level, string charclass, int level2, string charclass2, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma)
         {
             Name = name;
+            Level = level;
+            CharClass = charclass;
+            Level2 = level2;
+            CharClass2 = charclass2;
             Strength = strength;
             StrMod = Modifier(strength);
             Dexterity = dexterity;
